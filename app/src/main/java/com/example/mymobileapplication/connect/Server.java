dd94c849438d2d854d9e83c29dfa1aa3b2eaa9f6
sanghk38 <sanghk232@gmail.com>
2022-01-25 09:51:48 +0700

    First commit

---
+++ app/src/main/java/com/example/mymobileapplication/connect/Server.java
@@ -0,0 +1,22 @@
+package com.example.mymobileapplication.connect;
+
+public class Server {
+    public static final String BASE_URL = "http://192.168.1.13/mobilephp/getProducts.php";
+    public static  final String BAS_URL = "http://192.168.1.13/mobilephp/getSaleProduct.php";
+    public static  final String BAS_URL1 = "http://192.168.1.13/mobilephp/testp.php";
+    public static  final String Duongdanchitietdhonhang = "http://192.168.1.13/mobilephp/chitietdonhang.php";
+    public static  final String getFavorite = "http://192.168.1.13/mobilephp/favoriteadd.php";
+    public static  final String Duongdandonhang = "http://192.168.1.13/mobilephp/thongtinkhachhang.php";
+    public static String signup = "http://192.168.1.13/mobilephp/sign_up.php";
+    public static String signin = "http://192.168.1.13/mobilephp/sign_in.php";
+    public static String updatelike = "http://192.168.1.13/mobilephp/updatelike.php";
+    public static String yeuthich = "http://192.168.1.13/mobilephp/yeuthich.php";
+    public static String spmen = "http://192.168.1.13/mobilephp/getsanpham.php?page=";
+    public static String spwm = "http://192.168.1.13/mobilephp/getsanpham1.php?page=";
+    public static String pktt = "http://192.168.1.13/mobilephp/getsanpham2.php?page=";
+    public static String ttth = "http://192.168.1.13/mobilephp/getsanpham3.php?page=";
+    public static String sale = "http://192.168.1.13/mobilephp/getSale.php?page=";
+    public static String noel = "http://192.168.1.13/mobilephp/noel.php?page=";
+//    public static  final String favorite = "http://192.168.1.7/mobilephp/favorite.php";
+
+}
